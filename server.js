2fd65d7b91ffae19c263a575e433836e44173427
Nolan Yee <skinnyfatman@gmail.com>
2018-02-18 12:58:13 -0800

    hello world added to server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+app.get('/', function(req,res){
+  res.send('hello world');
+})
+
+app.listen(PORT, function(){console.log('listening on '+ PORT)});
